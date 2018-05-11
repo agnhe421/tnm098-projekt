96660a151da40672774304ed3ddffff923b8c162
V <hell_nam@hotmail.com>
2018-05-11 13:21:35 +0200

    Updated grouping function - working

--- js/mainPage.js
+++ js/mainPage.js
@@ -90,12 +90,12 @@ function mainPage(data)
 
 	
 	/***** VARIABLES FOR QUERIES ******/
-	var dataSubset = []; //to store the filtered subset
-	var idsInGroup = []; //to store the ids we want to filter
+	let dataSubset = []; //to store the filtered subset
+	let idsInGroup = []; //to store the ids we want to filter
 	
 	var actionType = "check-in"; //what action are we searching for?
 	var someDate = new Date('2014-06-06T06:52:00.000Z'); //09:04 - what time are we looking
-	var xCoord = 0, yCoord = 67; //coordinates
+	let xCoordList = [0, 87] ,  yCoordList = [67, 81]; //coordinates
 	var _queryType = "timeSpent"; //what statistics are we looking for?
 
     // lista med random ids från datan
@@ -137,24 +137,22 @@ function mainPage(data)
 		'theDate': someDate,
 		'theCoordinates' : { 'xCoord': 0, 'yCoord': 0 } 
 	};
-
-	
-	console.log(query);
-	//query.theCoordinates.push({xCoordinate: xCoord, yCoordinate:yCoord});
-	
-	//console.dir("Coordinates: " + queryConditions.theCoordinates);
 	
 	/***** FIND GROUPD ******/
-	//function(typeOf, theData, x, y, time, type)
+	
 	console.log("Looking for group..."); //after
 	let searchStartTime = performance.now();
 	
-	idsInGroup = getIds(data, xCoord, yCoord, someDate, actionType);
+	/******* TRYING TO SORT SHIT **********/
+
+	console.log("x: "+ xCoordList + " y: " + yCoordList);
+
+	let newData = findGroup(data, xCoordList, yCoordList, someDate, actionType);
+	console.log("Data from main:");
+	console.log(newData);
 	
 	searchingTime = performance.now() - searchStartTime;
 	console.log("Searched in " + searchingTime + " milliseconds.");
-	//idsInGroup = getIds(queryType, data,xCoord, yCoord, someDate, actionType);
-	dataSubset = data.filter(checkId, idsInGroup);
 
 	
 /**************************************************************************************************
@@ -164,59 +162,8 @@ function mainPage(data)
 **************************************************************************************************/
    
     /****** DRAW SCATTERPLOT ******/
-	updatePlot(dataSubset);
-	 /*
-   svg.selectAll("dot")
-        .data(dataSubset)
-		//.data(data.filter(checkId, list) )
-        .enter().append("circle")
-        .attr("r", function(d){
-			if(d.type == "check-in") return 5; 
-			return 2; 
-		})
-        .attr("cx", function(d){return x(d.X)})
-        .attr("cy", function(d) { return y(d.Y); })
-		.style("fill", function(d, i) {
-            
-			if( d.type == "check-in"){
-                //numberOfCheckIns++;
-                //console.log("Check-in at (" + d.X + ", " + d.Y + ")." );
-				return "red";
-            }
-			else {
-				let hour = d["Timestamp"] - OPENING_TIME_FRIDAY;
+	updatePlot(newData);
 
-				//console.log(d["Timestamp"]);
-
-				if( d["id"] == idsInGroup[0] ){
-					return color(Math.floor(hour/900000)-1);
-				}
-				else if( d["id"] == idsInGroup[1] ){
-					return color2(Math.floor(hour/900000)-1);
-				}
-				else if(d["id"] == idsInGroup[2]){
-					return color3(Math.floor(hour/900000)-1);
-				}
-				else if(d["id"] == idsInGroup[3]){
-					return color3(Math.floor(hour/900000)-1);
-				}
-				else if(d["id"] == idsInGroup[4]){
-					return color4(Math.floor(hour/900000)-1);
-				}
-				else if(d["id"] == idsInGroup[5]){
-					return color5(Math.floor(hour/900000)-1);
-				}		
-				else if(d["id"] == idsInGroup[6]){
-					return color6(Math.floor(hour/900000)-1);
-				}
-				else{
-					//console.log("Wrong coloring!");
-					return "black";
-				}
-			}
-		});
-		
-		*/
 
  // Add the X Axis
     svg.append("g")
@@ -228,6 +175,13 @@ function mainPage(data)
         .call(d3.axisLeft(y));
 
     console.log("DONE.");
+
+
+ /**************************************************************************************************
+*
+*									MEMBER FUNCTIONS
+*
+**************************************************************************************************/
 
 	
 	function updatePlot(data)
@@ -318,127 +272,197 @@ function mainPage(data)
 	}	
 	
 
-	//
-
-
-
-/**************************************************************************************************
-*
-*									MEMBER FUNCTIONS
-*
-**************************************************************************************************/
-
 /**
 * Function to specify how to compare entries for filter-function
 * Returns true if the data entries' ids are present in the list this
 * 
 ***/
-function checkId(value)
-{
-	for( var i = 0 ; i < this.length ; i++){
-		if(value["id"] == this[i])
-			return true;
-	}
-	return false;
-}
-
-
-/**
-* Function to check which ids satisfies query requirements
-* Returns empty array if nothing satisfies 
-* 
-* Returns an array with ids as integers(?)
-***/
-/*
-typeOf
-if(typeOf == "checkins"){
-if (typeOf == "timeSpent"){
-		newData = checkTimeSpent(theData);
-	}*/
-
-function getIds(theData, x, y, time, type) //skicka in vilken typeof value vi letar efter? 
-{
-	var newData = [];
-	//Find the groups 
-	
-	theData.forEach( function (d, i) {
-		if( checkCoordinates(d, x, y, time, type) ){
-				newData.push(theData[i]["id"]);
-		}});
-
-	//returning
-	if( newData.length == 0 )
-		console.log("No matches found.");
-	else
-		console.log("Group of size " + newData.length + " found.");
-	return newData; //return newData;
-}
-
-function pruneData(dataToPrune, xCoord, yCoord, time){
-
-}
-
-
-/**
-*
-* Returns empty array if nothing satisfies 
-* 
-* Returns an array with ids as integers(?)
-***/
-function checkTimeSpent( theData ){
-	
-}
-
-/**
-* Function to check if a data entry satisfies query conditions
-*
-* Returns true if entry fulfilles requirements
-* Else returns false
-***/
-function checkCoordinates(theData, x, y, time, type){
-	//console.log("Date: " + time);
-	//normalize time to current dates
-	var openingTime = new Date('2014-06-06T06:00:00.000Z');
-	var askedTime = time - openingTime;
-	//console.log("Query time: " + askedTime/600000 + "mins from opening time.");
-	
-	if( theData["type"] == "check-in" 
-		&& theData["X"] == x && theData["Y"] == y 
-		&& ( Math.abs(theData["Timestamp"].getTime() - time.getTime())) <= TIME_THRESHOLD ) //Math.floor((theData["Timestamp"]-openingTime)/600000) ==  Math.floor(askedTime/600000))
+	function checkId(value)
 	{
-			//console.log("Time: " + ( Math.abs(theData["Timestamp"].getTime() - time.getTime()) )/60000 + " minutes."); //divide by 60000 to normalize to minutes.
-			return true;
+		for( var i = 0 ; i < this.length ; i++){
+			if(value["id"] == this[i]["id"])
+				return true;
+		}
+		return false;
 	}
-	return false;
-}
 
-function checkPosition(point1, point2)
-{
-	if(point1 == point2)
-		return true; 
-	else return false; 
-}
 
-function checkAction(theType)
-{
-	if(theType == "check-in")
-		return true; 
-	else return false; 
-}
+	/**
+	* Function to check which ids satisfies query requirements
+	* Returns empty array if nothing satisfies 
+	* 
+	* Returns an array with ids as integers(?)
+	***/
+	/*
+	typeOf
+	if(typeOf == "checkins"){
+	if (typeOf == "timeSpent"){
+			newData = checkTimeSpent(theData);
+		}*/
 
-function timeCheck(time1, time2)
-{
-	if ( Math.abs(time1.getTime() - time2.getTime()) <= TIME_THRESHOLD ) 
-		return true; 
-	else return false; 
-}
+	function findGroup(theData,  xCoords,  yCoords, time, type) //skicka in vilken typeof value vi letar efter? 
+	{
+		var newData = [];
+		let idxList = [];
+
+		//Find the ids at point 
+		theData.forEach( function(dataPoint) 
+		{
+			if( checkCoordinates(dataPoint, xCoords[0], yCoords[0], time, type) ){
+					newData.push(dataPoint);
+					
+			}
+		})
+
+		//find all actions of an Id
+		newData = theData.filter(checkId, newData);
+
+		//if these was the last coords: return
+		if( xCoords.length == 1){
+			console.log("Group of size " + newData.length + " found.");
+			return newData;
+		}
+
+		//if not: make recursive call
+		else{
+			console.log("Recursive call");
+			
+			newData = findGroupAgain(newData, xCoords.slice(1), yCoords.slice(1))
+			
+			return newData;	
+		}
+		
+	}
+
+	function findGroupAgain(theData, xCoords, yCoords){
+		var newData = [];
+		console.log("x: " + xCoords[0] + " y: " + yCoords[0]);
+		
+		//Find the ids at point 
+		theData.forEach( function(dataPoint) 
+		{
+			if( checkPosition(dataPoint["X"], xCoords[0]) 
+				&& checkPosition(dataPoint["Y"], yCoords[0]) ){
+					newData.push(dataPoint);
+					console.log("pushing!");
+			}
+		})
+
+		//find all actions of an Id
+		newData = theData.filter(checkId, newData);
+		console.log(newData);
+
+		//if last coords: return
+		if( xCoords.length == 1){
+			console.log("Group of size " + newData.length + " found.");
+			
+			return newData;
+		}
+		//if not: make recursive call
+		else{
+			console.log("Recursive call second level.")
+			
+			//recursive call
+			newData = findGroup(newData, xCoords.slice(1), yCoords.slice(1));
+			
+			return newData;
+		}
+	}
+
+
+	function retrieveIds(theList) {
+		listOfIds = [];
+
+		theList.forEach(function (d) {
+			listOfIds.push(d["id"])
+		})
+
+		return listOfIds;
+	}
+
+	function pruneData(list, xCoordList, yCoordList){
+		var newList = list;
+
+		console.log("Length " + list.length);
+		console.log(list);
+		console.log(newList);
+
+		let i = 0;
+		while(i < newList.length){
+			console.log("Comparing ("+ newList[i]["X"] + ", " + newList[i]["Y"] 
+						+ ") with (" + xCoordList[0] + ", " + yCoordList[0] + ").");
+		
+
+			if(!checkPosition(xCoordList[0], newList[i]["X"]) 
+			&& !checkPosition(yCoordList[0], newList[i]["Y"])) {
+					console.log("Checked, not equal!");
+					console.log("i: " + i)
+					console.log("before splice: " + newList.length); 
+						newList.splice(i,1);
+						console.log("after splice: " + newList.length); 
+			}
+			else i++; 
+		}
+			return newList;
+		// }
+		
+	}
+
+
+	/**
+	*
+	* Returns empty array if nothing satisfies 
+	* 
+	* Returns an array with ids as integers(?)
+	***/
+	function checkTimeSpent( theData ){
+		
+	}
+
+	/**
+	* Function to check if a data entry satisfies query conditions
+	*
+	* Returns true if entry fulfilles requirements
+	* Else returns false
+	***/
+	function checkCoordinates(theData, x, y, time, type){
+		//console.log("Date: " + time);
+		//normalize time to current dates
+		var openingTime = new Date('2014-06-06T06:00:00.000Z');
+		var askedTime = time - openingTime;
+		//console.log("Query time: " + askedTime/600000 + "mins from opening time.");
+		
+		if( /*theData["type"] == "check-in" 
+			&&*/ theData["X"] == x && theData["Y"] == y 
+			&& ( Math.abs(theData["Timestamp"].getTime() - time.getTime())) <= TIME_THRESHOLD ) //Math.floor((theData["Timestamp"]-openingTime)/600000) ==  Math.floor(askedTime/600000))
+		{
+				//console.log("Time: " + ( Math.abs(theData["Timestamp"].getTime() - time.getTime()) )/60000 + " minutes."); //divide by 60000 to normalize to minutes.
+				return true;
+		}
+		return false;
+	}
+
+	function checkPosition(point1, point2)
+	{
+		if(point1 == point2)
+			return true; 
+		else return false; 
+	}
+
+	function checkAction(theType)
+	{
+		if(theType == "check-in")
+			return true; 
+		else return false; 
+	}
+
+	function timeCheck(time1, time2)
+	{
+		if ( Math.abs(time1.getTime() - time2.getTime()) <= TIME_THRESHOLD ) 
+			return true; 
+		else return false; 
+	}
 
 }
 	
-	
- /*** DEFINE THE LINE ******/
-   /* var valueline = d3.line()
-     //   .x(function(d) { return x(d.Timestamp ) })
-   .x(function(d) { return x(d.X )})          
-	.y(function(d) { return y(d.Y); });
-*/
+
