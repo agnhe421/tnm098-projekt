096bab08e917a79bf7c5d7f6fbb41d272e55adc1
Agnes <agnhe421@student.liu.se>
2019-06-16 22:10:19 +0200

    clean up and small fixes

--- js/pieChart.js
+++ js/pieChart.js
@@ -1,21 +1,12 @@
-var width = 350
-height = 350
-margin = 40
+var width = 500
+height = 500
+margin = 100
 
 var radius = Math.min(width, height) / 2 - margin
 
 function pieChart(test) {
     this.test = test;
 }
-
-console.warn('initializing piechart')
-    //Note to self:
-    //Det fungerar inte att byta dataset av någon anlednign - Fixa det
-    //Koppla knapparna för pie-chartet till de stora knapparna för dagarna
-    //Se till så att datan kommer från faktisk data och inte hårdkodat i componenten
-    //Bestäm vad som ska visas i bar-charten längst ner till höger 
-    //Koppla knapparna för bar-charten till de stora kanpparna för dagarna
-    //Generera faktisk data 
 
 // append the svg object to the div called 'my_dataviz'
 var svg = d3.select("#pieChart")
@@ -26,15 +17,24 @@ var svg = d3.select("#pieChart")
 .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 // create 2 data_set
-var data11 = {a: 9, b: 20, c:30, d:8, e:12}
-var data22 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
+var groupSizesFriday = { alone: 24016, twoToThree: 8532, threeToFour:6430, moreThanFour:2721 }
+var groupSizesSaturday = { alone: 52838, twoToThree: 11375, threeToFour:6168, moreThanFour:2379 }
+var groupSizesFriday = { alone: 67791, twoToThree: 12841, threeToFour:6341, moreThanFour:1882 }
+
 
 // set the color scale
 var color = d3.scaleOrdinal()
 .domain(["a", "b", "c", "d", "e", "f"])
 .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
 
+// // shape helper to build arcs:
+// var arcGenerator = d3.arc()
+//   .innerRadius(0)
+//   .outerRadius(radius)
 
+  var arcGenerator = d3.arc()
+	.innerRadius(radius * 1.3)
+  .outerRadius(radius * 1.3);
 // A function that create / update the plot for a given variable:
 function updatePieChart(data) {
 
@@ -67,6 +67,15 @@ u.enter()
 .style("stroke-width", "2px")
 .style("opacity", 1)
 
+svg
+  .selectAll('mySlices')
+  .data(data_ready)
+  .enter()
+  .append('text')
+  .text(function(d){ return d.data.key})
+  .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+  .style("text-anchor", "middle")
+  .style("font-size", 17)
 // remove the group that is not present anymore
 u.exit()
 .remove()
@@ -74,4 +83,4 @@ u.exit()
 }
 
 // Initialize the plot with the first dataset
-updatePieChart(data11)
+updatePieChart(groupSizesFriday)
