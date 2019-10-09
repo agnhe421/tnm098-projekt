0efe26aaead4499e1bd344b8252b6898d28470e7
Agnes <agnhe421@student.liu.se>
2019-10-09 22:25:56 +0200

    fix code

--- js/attractions.js
+++ js/attractions.js
@@ -4,10 +4,9 @@ var barMargin = { top: 10, right: 30, bottom: 30, left: 60 },
   barHeight = 450 - barMargin.left - barMargin.right
 
 function attractionsPlot(data) {
-  console.warn(data)
   updateAttractionsPlot(data)
 }
-  
+
 //  append the svg object to the body of the page
 var svgAttractions = d3.select("#attractionsPlot")
   .append("svg")
@@ -17,29 +16,36 @@ var svgAttractions = d3.select("#attractionsPlot")
   .attr("transform",
     "translate(" + barMargin.left + "," + barMargin.top + ")");
 
+
 function updateAttractionsPlot(data) {
+    // Add Y axis
+  var y = d3.scaleLinear()
+  .domain([0, 60])
+  .range([barHeight, 0]);
+
+  svgAttractions.append("g")
+  .attr("class", "myYaxis")
+  .call(d3.axisLeft(y));
 
   var x = d3.scaleBand()
     .range([0, barWidth])
-    .domain(data.map(function (d) { 
-       return d.name; 
-     }))
+    .domain(data.map(function (d) {
+      return d.name;
+    }))
     .padding(0.2);
   svgAttractions.append("g")
     .attr("transform", "translate(0," + barHeight + ")")
     .call(d3.axisBottom(x))
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 60])
-    .range([barHeight, 0]);
-
-    svgAttractions.append("g")
-    .attr("class", "myYaxis")
-    .call(d3.axisLeft(y));
-
   var bar = svgAttractions.selectAll("rect")
     .data(data)
+
+  var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .text('tooltip')
 
   bar.enter()
     .append("rect")
@@ -49,8 +55,28 @@ function updateAttractionsPlot(data) {
     .attr("x", function (d) { return x(d.name); })
     .attr("y", function (d) { return y(d.ratio); })
     .attr("width", x.bandwidth())
-    .attr("height", function (d) { return barHeight - y(d.ratio);})
+    .attr("height", function (d) { return barHeight - y(d.ratio); })
     .attr("fill", "#69b3a2")
     .style('pointer-events', 'all')
+
+    svgAttractions.selectAll('rect')
+    .data(data)
+    .on("mouseover", function(d) {
+      d3.select(this)
+      .attr("fill", "orange");
+      tooltip.html('name: ' + d.name + '<br>' + 'ratio: '+ d.ratio + '<br>' + 'checkins: ' + d.checkins )
+      tooltip.style('background-color', 'LightSteelBlue')
+      .style('border-radius', '5px')
+      .style('padding', '5px');	
+
+      return tooltip.style("visibility", "visible");
+    })
+    .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+    .on("mouseout", function() {
+      d3.select(this)
+      .attr("fill", "#69b3a2");
+      return tooltip.style("visibility", "hidden");
+    });
+
 }
 
